e51bd58a3668458ccbffe987330f54329e2e0f04
Adam Gravitis <adam@adamgravitis.ca>
2016-04-06 10:45:35 -0400

    Added tweaks/comment

--- art.js
+++ art.js
@@ -1,3 +1,12 @@
+/**
+ * Auto-generate art in the style of Gene Davis.  
+ *
+ *
+ * (Work in progress; no UX except for the output just now)
+ * (petty cool though)
+ * (also, solid work by the chroma.js people on that library, especialy the bezier and lightness correction stuff)
+ */
+
 var Art = function(canvas, width, height) {
     this.canvas = canvas;
 
@@ -7,13 +16,16 @@ var Art = function(canvas, width, height) {
 
     this.ctx = this.canvas.getContext("2d");
 
-
     var totalColored = 0;
-    var numColors = 30;
-    var colors = chroma.scale(['aqua', 'royalBlue', 'navy']).colors(numColors);
+    var numColors = 50;
+    var colors = chroma.scale(['rgb(45, 186, 242)', 'royalBlue', 'navy']).correctLightness(true).colors(numColors);
+
+    var narrowColor = 5;
+    var wideColor = 100;
+
     for (var i = 0; totalColored < this.canvas.width; i++) {
-	var lineWidth = Math.floor(Math.random() * (Math.random() > 0.95 ? 400 : 10));
-	var color = colors[lineWidth > 30 ? Math.floor(numColors*0.4) + Math.floor(numColors*0.6 * Math.random()) : Math.floor(numColors * Math.random())];
+	var lineWidth = Math.floor(Math.random() * (Math.random() > 0.97 ? wideColor * 2 : narrowColor * 2));
+	var color = colors[lineWidth > wideColor / 2 ? Math.floor(numColors*0.6) + Math.floor(numColors*0.4 * Math.random()) : Math.floor(numColors * Math.random())];
 	this.drawLine(color, totalColored, lineWidth);
 	totalColored += lineWidth;
     }
